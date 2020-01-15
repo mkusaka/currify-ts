c5196153df84980f8224150654278df10148b65b
mkusaka <hinoshita1992@gmail.com>
2020-01-15 20:36:17 +0900

    imp type test

--- index.ts
+++ index.ts
@@ -1,13 +1,26 @@
-export const currify = <F extends Function>(fn: F) => {
-  return <K>(arg: K) => resc(fn, fn.length, arg)
-}
+export const currify = <F extends (...args: any[]) => any>(fn: F) => {
+  return <K>(arg: K) => resc(fn, fn.length, arg);
+};
 
-const resc = <F extends Function, T>(fn: F, arglength: number, ...arg: T[]) => {
+const resc = <F extends (...args: any[]) => any, T>(
+  fn: F,
+  arglength: number,
+  ...arg: Parameters<typeof fn>[number][]
+) => {
   if (arglength === 1) {
-    return fn(...arg)
+    return fn(...arg);
   }
-  // TODO: infer better argument type.
-  return (arg2: any) => resc(fn, arglength - 1, ...arg, arg2)
-}
+  const parameterPosition = fn.length - arglength;
+  type Arg = Parameters<typeof fn>[typeof parameterPosition];
+  return (arg2: Arg) => resc(fn, arglength - 1, ...arg, arg2);
+};
 
 export default currify;
+
+/*
+function hoge(a: string, b: number) {
+  return a
+}
+
+type P = Parameters<typeof hoge>[number]
+ */
